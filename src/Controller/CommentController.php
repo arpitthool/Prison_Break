6ac4bf38883a54b51439d0fcd64fa8f5e35e7bbe
arpitthool <arpitthool1@gmail.com>
2021-01-18 19:13:18 +0530

    learned about Service, which are basically Objects that do work, and used the Logger service

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,18 +10,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/comments/{id}/vote/{direction}")
+     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote( $id, $direction)
+    public function commentVote( $id, $direction, LoggerInterface $logger)
     {   
         // todo use id to query to database
         $currentVoteCount = 0;
 
         if ( $direction === 'up') {
+            $logger->info("voting up!");
             $currentVoteCount = rand( 7,100);
-        }
-        elseif( $direction == 'down'){
-            $currentVoteCount == rand( 0,5);
+        } else {
+            $logger->info("voting down!");
+            $currentVoteCount == rand( 1,5);
         }
 
         return $this->json([
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage()//Environment $twigEnvironment)
     {
+        //example of using the Twig service
+        // $html = $twigEnvironment->render('question/homepage.html.twig');
+        // return new Response($html);
         return $this->render('question/homepage.html.twig');
     }
 
